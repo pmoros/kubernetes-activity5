bd036cb4253398779c6e69eaea941a6416591c3e
Paul Moros <pmoros@outlook.com>
2023-03-20 12:09:53 -0500

    feat(infrastructure): add subnet for aks

--- infrastructure/dev/networks.tf
+++ infrastructure/dev/networks.tf
@@ -4,4 +4,14 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.vnet_address_space
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet-${var.project}-${var.environment}-${var.location}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.aks_subnets_address_prefixes
+
 }
